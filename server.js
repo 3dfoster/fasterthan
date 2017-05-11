011bca06d918f0c87fd8a7e384c7d4c08c438786
David Foster <faster@ucdavis.edu>
2017-05-11 13:30:36 -0700

    process env

--- server.js
+++ server.js
@@ -157,6 +157,6 @@ server.on('request', (req, res) => {
     console.log('Request Body: \t' + body)
     console.log('===================================')
   })
-}).listen(8000)
+}).listen(env.process.PORT)
 
 console.log("Server started at http://localhost:" + server.address().port)
